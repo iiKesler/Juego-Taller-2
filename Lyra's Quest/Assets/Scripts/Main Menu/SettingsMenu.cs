e990fce606cd30fa5a007d460e8dbffbe2220273
Kusseler <andresmartinezbedoya@gmail.com>
2023-04-24 15:20:38 -0500

    Menu Principal Funcionando
    Desarrollo del menu principal con funciones como: Pantalla completa si o no, mas resoluciones de pantalla, volumen

---
+++ Lyra's Quest/Assets/Scripts/Main Menu/SettingsMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Main_Menu
+{
+    public class SettingsMenu : MonoBehaviour
+    {
+        public AudioMixer audioMixer;
+        private Resolution[] _resolutions;
+
+        public TMP_Dropdown resolutionDropdowns;
+
+        private void Start()
+        {
+            _resolutions = Screen.resolutions;
+            resolutionDropdowns.ClearOptions();
+
+            var options = new List<string>();
+            
+            var currentResolutionIndex = 0;
+            for (var i = 0; i < _resolutions.Length; i++)
+            {
+                var option = _resolutions[i].width + " x " + _resolutions[i].height + " @ " + _resolutions[i].refreshRate + "hz";
+                options.Add(option);
+
+                if (_resolutions[i].Equals(Screen.currentResolution))
+                {
+                    currentResolutionIndex = i;
+                }
+            }
+            
+            resolutionDropdowns.AddOptions(options);
+            resolutionDropdowns.value = currentResolutionIndex;
+            resolutionDropdowns.RefreshShownValue();
+        }
+
+        public void SetVolume(float volume)
+        {
+            audioMixer.SetFloat("volume", Mathf.Log10(volume)*20);
+        }
+
+        public void SetQuality(int qualityIndex)
+        {
+            QualitySettings.SetQualityLevel(qualityIndex);
+        }
+
+        public void SetFullscreen(bool isFullscreen)
+        {
+            Screen.fullScreen = isFullscreen;
+        }
+    }
+}
